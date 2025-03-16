c74ab2044de0028a729986cfd05c9b2c41bcf18e
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-03-16 09:27:25 +0200

    add users model & tests

--- users/tests/test_managers.py
+++ users/tests/test_managers.py
@@ -65,8 +65,8 @@ class TestUserManager(TestCase):
         with self.assertRaises(ValueError):
             self.user_manager.create_user({**self.valid_user_data, "first_name": ""})
 
-    def test_valid_create_superuser(self):
-        superuser = self.user_manager.create_superuser(self.valid_superuser_data)
+    def test_create_superuser(self):
+        superuser = self.user_manager.create_superuser(**self.valid_superuser_data)
 
         self.assertEqual(
             superuser.email,

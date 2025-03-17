41af6e032712c62d27b24987613ce2d1534a2524
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-03-17 17:56:28 +0200

    add projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(_("Name"), max_length=150)
+    description = models.TextField(_("Description"))
+    created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
+
+    def __str__(self):
+        return self.name

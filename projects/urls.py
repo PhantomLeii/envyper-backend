cc08fca87e83999174fb22e6e9d391de451e1421
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-03-18 11:33:50 +0200

    add project detail routes

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import ProjectsAPIView
+from .views import ProjectsAPIView, ProjectDetailAPIView
 
 
-urlpatterns = [path("", ProjectsAPIView.as_view(), name="projects")]
+urlpatterns = [
+    path("", ProjectsAPIView.as_view(), name="projects"),
+    path("<int:pk>/", ProjectDetailAPIView.as_view(), name="projects-detail"),
+]

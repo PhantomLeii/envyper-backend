62a985a21ce504ac20704673e46501798bb745e3
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-03-18 09:31:06 +0200

    update projects model

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Projects(models.Model):
     name = models.CharField(_("Name"), max_length=150)
     description = models.TextField(_("Description"))
+    updated_at = models.DateTimeField(_("Updated At"), auto_now=True)
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
 
     def __str__(self):

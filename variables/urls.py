31508ab940dede6e3e9b96f7a065802714b56c06
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-03-19 11:18:31 +0200

    add variables routes

--- variables/urls.py
+++ variables/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
+from .views import VariablesAPIView, VariableDetailAPIView
 
 
-urlpatterns = []
+urlpatterns = [
+    path("<int:project_id>/", VariablesAPIView.as_view(), name="variables"),
+    path(
+        "<int:project_id>/<int:pk>/",
+        VariableDetailAPIView.as_view(),
+        name="variable-detail",
+    ),
+]
